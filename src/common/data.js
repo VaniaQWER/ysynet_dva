ffef96208ebed9810ecf77b2fa769484586d969b
vania <6058960@qq.com>
2018-07-24 13:28:33 +0800

    mock数据
    mock数据

---
+++ src/common/data.js
@@ -0,0 +1,254 @@
+export const createData = () => (
+  [
+    {
+      id: 1, // id
+      approvalNo: '国药准字Z20123084', // 批准文号
+      productName: '金前感冒胶囊', // 产品名称
+      spec: '每粒装0.3g', // 规格
+      fmodal: '胶囊剂',  // 剂型
+      productCompany: '保定天浩制药有限公司', // 生产单位
+      medicinalCode: '86902551000259', // 药品编码
+      remark: '' // 药品编码备注
+    },
+    {
+      id: 2,
+      approvalNo: '国药准字Z20123085',
+      productName: '夏荔芪胶囊',
+      spec: '每粒装0.45g',
+      fmodal: '胶囊剂',
+      productCompany: '石家庄以岭药业股份有限公司',
+      medicinalCode: '86902767000852',
+      remark: ''
+    },
+    {
+      id: 3,
+      approvalNo: '国药准字H20064705',
+      productName: '三溴片',
+      spec: '溴化钾0.12g,溴化钠0.12g，溴化铵0.06g',
+      fmodal: '片剂',
+      productCompany: '陕西西岳制药有限公司',
+      medicinalCode: '86902439000609',
+      remark: ''
+    },
+    {
+      id: 4,
+      approvalNo: '国药准字H61020562',
+      productName: '口服补液盐散(Ⅰ)',
+      spec: '14.75克',
+      fmodal: '散剂',
+      productCompany: '陕西春晖药业有限公司',
+      medicinalCode: '86902422000128',
+      remark: ''
+    },
+    {
+      id: 5,
+      approvalNo: '国药准字H20123308',
+      productName: '胶体果胶铋',
+      spec: '－－－－',
+      fmodal: '原料',
+      productCompany: '四川得恩德药业有限公司',
+      medicinalCode: '86902133000028',
+      remark: ''
+    },
+    {
+      id: 6,
+      approvalNo: '国药准字H20123298',
+      productName: '枸橼酸芬太尼注射液',
+      spec: '10ml:0.5mg(以芬太尼计)',
+      fmodal: '注射剂',
+      productCompany: '国药集团工业有限公司廊坊分公司',
+      medicinalCode: '86902581000045',
+      remark: ''
+    },
+    {
+      id: 7,
+      approvalNo: '国药准字H20123300',
+      productName: '谷胱甘肽',
+      spec: '－－－－',
+      fmodal: '原料药',
+      productCompany: '山东金城生物药业有限公司',
+      medicinalCode: '86909638000010',
+      remark: ''
+    },
+    {
+      id: 8,
+      approvalNo: '国药准字H20123297',
+      productName: '枸橼酸芬太尼注射液',
+      spec: '2ml:0.1mg(以芬太尼计)',
+      fmodal: '注射剂',
+      productCompany: '国药集团工业有限公司廊坊分公司',
+      medicinalCode: '86902581000052',
+      remark: ''
+    },
+    {
+      id: 9,
+      approvalNo: '国药准字H20073812',
+      productName: '硫普罗宁片',
+      spec: '0.1g',
+      fmodal: '片剂',
+      productCompany: '成都倍特药业有限公司',
+      medicinalCode: '86902013000636',
+      remark: ''
+    },
+    {
+      id: 10,
+      approvalNo: '国药准字S20060078',
+      productName: 'A群C群脑膜炎球菌多糖结合疫苗',
+      spec: '按标示量复溶后每瓶0.5ml,每1次人用剂量0.5ml,含A群结合多糖不少于10ug,含C群结合多糖不少于10ug,乳糖5.0-7.0mg。',
+      fmodal: '注射剂',
+      productCompany: '罗益(无锡)生物制药有限公司',
+      medicinalCode: '86901553000014',
+      remark: ''
+    },
+    {
+      id: 11,
+      approvalNo: '国药准字H20070217',
+      productName: '注射用复方甘草酸苷',
+      spec: '甘草酸苷80mg、甘氨酸800mg、盐酸半胱氨酸40mg',
+      fmodal: '注射剂(冻干粉针剂)',
+      productCompany: '北京四环空港药业科技有限公司',
+      medicinalCode: '86900234000039',
+      remark: ''
+    },
+    {
+      id: 12,
+      approvalNo: '国药准字H20070218',
+      productName: '注射用复方甘草酸苷',
+      spec: '甘草酸苷40mg、甘氨酸400mg、盐酸半胱氨酸20mg',
+      fmodal: '注射剂(冻干粉针剂)',
+      productCompany: '北京四环空港药业科技有限公司',
+      medicinalCode: '86900234000046',
+      remark: ''
+    },
+    {
+      id: 13,
+      approvalNo: '国药准字H20073805',
+      productName: '枸橼酸钾',
+      spec: '----',
+      fmodal: '原料药',
+      productCompany: '日照鲁信金禾生化有限公司',
+      medicinalCode: '86904061000033',
+      remark: ''
+    },
+    {
+      id: 14,
+      approvalNo: '国药准字H20073782',
+      productName: '黄芩苷',
+      spec: '----',
+      fmodal: '原料药',
+      productCompany: '山东三精制药有限公司',
+      medicinalCode: '86908171000013',
+      remark: ''
+    },
+    {
+      id: 15,
+      approvalNo: '国药准字H20073732',
+      productName: '乳果糖浓溶液',
+      spec: '----',
+      fmodal: '----',
+      productCompany: '莱阳市江波制药有限责任公司',
+      medicinalCode: '86904201000251',
+      remark: ''
+    },
+    {
+      id: 16,
+      approvalNo: '国药准字Z37020726',
+      productName: '药艾条',
+      spec: '29g/支',
+      fmodal: '灸剂',
+      productCompany: '山东宏济堂医药有限公司中药厂',
+      medicinalCode: '86903986000067',
+      remark: ''
+    },
+    {
+      id: 17,
+      approvalNo: '国药准字Z37020725',
+      productName: '生三七散',
+      spec: '5g/瓶',
+      fmodal: '散剂',
+      productCompany: '山东宏济堂医药有限公司中药厂',
+      medicinalCode: '86903986000050',
+      remark: ''
+    },
+    {
+      id: 18,
+      approvalNo: '国药准字H52020858',
+      productName: '乳酸环丙沙星氯化钠注射液',
+      spec: '100ml:0.1g(以环丙沙星计)',
+      fmodal: '注射剂',
+      productCompany: '贵州百灵企业集团制药股份有限公司',
+      medicinalCode: '86905437000183',
+      remark: ''
+    },
+    {
+      id: 19,
+      approvalNo: '国药准字H20073737',
+      productName: '卡络磺钠',
+      spec: '----',
+      fmodal: '原料药',
+      productCompany: '浙江普洛康裕制药有限公司',
+      medicinalCode: '86904685000587',
+      remark: ''
+    },
+    {
+      id: 20,
+      approvalNo: '国药准字H20033672',
+      productName: '苯甲醇注射液',
+      spec: '3.2ml:28.8mg',
+      fmodal: '注射剂',
+      productCompany: '正大天晴药业集团股份有限公司',
+      medicinalCode: '86901523000075',
+      remark: ''
+    },
+    {
+      id: 21,
+      approvalNo: '国药准字H52020860',
+      productName: '替硝唑氯化钠注射液',
+      spec: '200ml:0.8g',
+      fmodal: '注射剂',
+      productCompany: '贵州百灵企业集团制药股份有限公司',
+      medicinalCode: '86905437000909',
+      remark: ''
+    },
+    {
+      id: 22,
+      approvalNo: '国药准字H52020856',
+      productName: '乳酸环丙沙星氯化钠注射液',
+      spec: '100ml:0.2g(以环丙沙星计)',
+      fmodal: '注射剂',
+      productCompany: '贵州百灵企业集团制药股份有限公司',
+      medicinalCode: '86905437000121',
+      remark: ''
+    },
+    {
+      id: 23,
+      approvalNo: '国药准字H52020854',
+      productName: '氟康唑氯化钠注射液',
+      spec: '100ml:氟康唑0.2g与氯化钠0.9g',
+      fmodal: '注射剂',
+      productCompany: '贵州百灵企业集团制药股份有限公司',
+      medicinalCode: '86905437000688',
+      remark: ''
+    },
+    {
+      id: 24,
+      approvalNo: '国药准字H52020861',
+      productName: '氧氟沙星氯化钠注射液',
+      spec: '100ml:氧氟沙星0.2g与氯化钠0.9g',
+      fmodal: '注射剂',
+      productCompany: '注射剂',
+      medicinalCode: '86905437000381',
+      remark: ''
+    },
+    {
+      id: 25,
+      approvalNo: '国药准字H52020857',
+      productName: '乳酸环丙沙星氯化钠注射液',
+      spec: '250ml:0.25g(以环丙沙星计)',
+      fmodal: '注射剂',
+      productCompany: '贵州百灵企业集团制药股份有限公司',
+      medicinalCode: '86905437000152',
+      remark: ''
+    },
+  ]
+)
