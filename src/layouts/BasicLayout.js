58cf884ddc85e08eb8bd379da49d98fcbadcc030
vania <6058960@qq.com>
2018-07-24 22:50:43 +0800

    【create】新增系统切换
    【create】新增系统切换

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { Route, Switch } from 'dva/router';
-import { Layout,Icon } from 'antd';
+import { Layout, Icon, Row, Col, Tooltip  } from 'antd';
 import { connect } from 'dva';
 import SiderMenu from '../components/SiderMenu';
 import styles from './style.css';
@@ -41,16 +41,28 @@ class BasicLayout extends PureComponent {
         </Sider>
         <Content>
           <Header className={`${styles.header}`} style={{ marginBottom: 3,padding: 0 }}>
-            <Icon 
-              onClick={() => {
-                const { collapsed } = this.state;
-                this.setState({
-                  collapsed: !collapsed
-                })
-              }}
-              className='ysyenert-header-icon ysynet-collapsed'
-              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} 
-            /> {title.mainTitle} </Header>
+            <Row>
+              <Col span={4}>
+                <Icon 
+                  onClick={() => {
+                    const { collapsed } = this.state;
+                    this.setState({
+                      collapsed: !collapsed
+                    })
+                  }}
+                  className='ysyenert-header-icon ysynet-collapsed'
+                  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} 
+                /> {title.mainTitle}
+              </Col>
+              <Col span={18} style={{textAlign: 'right'}}>
+                <Tooltip title="子系统切换">
+                  <Icon type="sync" className={styles.icon} onClick={() => this.props.history.push({
+                    pathname: '/subSystem'
+                  })}/> 
+                </Tooltip>
+              </Col>
+            </Row>
+          </Header>
           <Header className={`${styles.subHeader}`}>
             {title.subTitle}
           </Header>
