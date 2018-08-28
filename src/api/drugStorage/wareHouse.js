fd5c7d37e6cff66ac76114025b59deb0e8b86ac4
JsCat1 <18907242075@163.com>
2018-08-28 10:10:50 +0800

    【fix】

---
+++ src/api/drugStorage/wareHouse.js
@@ -0,0 +1,11 @@
+/*
+    药库 - 入库单管理
+
+*/
+
+
+import {_local} from '../local';
+export default {
+  depotinstoreList: `${_local}depot/depotinstore/list`,    //库存列表
+  depotinstoreInfo: `${_local}depot/depotinstore/info`,    //库存详情
+}
